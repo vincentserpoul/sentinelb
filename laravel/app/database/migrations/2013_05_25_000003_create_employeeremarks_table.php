6fc414fbc82f11a6ff1df8faf90b985428ef1e77
Jean Jacques Serpoul <serpoul@gmail.com>
2013-12-16 19:51:38 +0100

    Field remark from employee_remarks text(1000) to text(5000) - Import from old app

--- laravel/app/database/migrations/2013_05_25_000003_create_employeeremarks_table.php
+++ laravel/app/database/migrations/2013_05_25_000003_create_employeeremarks_table.php
@@ -17,7 +17,7 @@ class CreateEmployeeremarksTable extends Migration
             $table->increments('id')->unsigned();
             $table->integer('employee_id')->unsigned();
             $table->integer('globalevent_period_id')->nullable()->default(NULL)->unsigned();
-            $table->string('remark', 1000)->nullable()->default(NULL);
+            $table->string('remark', 5000)->nullable()->default(NULL);
             $table->timestamps();
             $table->integer('user_id')->nullable()->default('1')->unsigned();
         });
