0c6c8eff0af12b7feb2707af882ce4103213570d
vserpoul <vincent@serpoul.com>
2014-03-18 07:57:55 +0800

    db qa

--- laravel/app/config/qa/database.php
+++ laravel/app/config/qa/database.php
@@ -57,7 +57,7 @@ return array(
             'host'      => 'localhost',
             'database'  => 'centuryevergreen',
             'username'  => 'root',
-            'password'  => 'dev',
+            'password'  => 'Kiwi1769',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
